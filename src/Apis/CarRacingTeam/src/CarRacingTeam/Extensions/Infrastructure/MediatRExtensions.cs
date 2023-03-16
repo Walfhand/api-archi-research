a60c5e81c50a76c0993011a51ad7efd5569cd315
Sebastien <sva@eonix.be>
2023-03-16 19:52:15 +0100

    update mediator

--- src/Apis/CarRacingTeam/src/CarRacingTeam/Extensions/Infrastructure/MediatRExtensions.cs
+++ src/Apis/CarRacingTeam/src/CarRacingTeam/Extensions/Infrastructure/MediatRExtensions.cs
@@ -3,13 +3,14 @@ using Engine.Mongo;
 using Engine.Validation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace CarRacingTeam.Extensions.Infrastructure;
 internal static class MediatRExtensions
 {
     public static IServiceCollection AddCustomMediatR(this IServiceCollection services)
     {
-        services.AddMediatR(typeof(MediatRExtensions).Assembly);
+        services.AddMediatR(x => x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehavior<,>));
